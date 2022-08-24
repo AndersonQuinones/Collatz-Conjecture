3c2795ec6ce7e99eab455ef5c731d4dbbb17818c
Anderson <Anderson.quinones10@gmail.com>
2022-08-24 12:01:56 -0400

    updated the yml

--- collatz-conjecture/src/components/chart.js
+++ collatz-conjecture/src/components/chart.js
@@ -1,60 +1,56 @@
 import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
-const Chart = () => {
-  return (
-    <div>
-      Hello
-    </div>
-  )
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
-export default Chart;
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-//import {
-  //   Chart as ChartJS,
-  //   CategoryScale,
-  //   LinearScale,
-  //   BarElement,
-  //   Title,
-  //   Tooltip,
-  //   Legend,
-  // } from 'chart.js';
-  // import { Bar } from 'react-chartjs-2';
-  // import faker from 'faker';
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => getRandomArbitrary(0, 100)),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => getRandomArbitrary(0, 100)),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
-  // ChartJS.register(
-  //   CategoryScale,
-  //   LinearScale,
-  //   BarElement,
-  //   Title,
-  //   Tooltip,
-  //   Legend
-  // );
-
-  // export const options = {
-  //   responsive: true,
-  //   plugins: {
-  //     title: {
-  //       display: true,
-  //       text: 'Chart.js Bar Chart',
-  //     },
-  //   },
-  // };
-
-  // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-  // export const data = {
-  //   labels,
-  //   datasets: [
-  //     {
-  //       label: 'Dataset 1',
-  //       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-  //       backgroundColor: 'rgba(255, 99, 132, 0.5)',
-  //     },
-  //     {
-  //       label: 'Dataset 2',
-  //       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-  //       backgroundColor: 'rgba(53, 162, 235, 0.5)',
-  //     },
-  //   ],
-  // };
+export default function Chart() {
+  return <Bar options={options} data={data} />;
+}
